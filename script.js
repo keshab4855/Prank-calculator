d6dec66b4b4e75cd813a0e9108333a72b578b397
keshab4855 <keshabroka123@gmail.com>
2022-05-19 16:02:35 +1000

    error handling

--- script.js
+++ script.js
@@ -7,12 +7,58 @@
 const buttons = document.querySelectorAll("button");
 const displayEl = document.querySelector(".display-box");
 
+const operators = ["+", "-", "*", "/"];
+
 const buttonsArr = Array.from(buttons);
+
+let lastOperator = "";
 
 let strToDisplay = "";
 buttonsArr.map((btns) => {
   btns.addEventListener("click", () => {
     const val = btns.innerText;
+
+    if (val === "AC") {
+      strToDisplay = "";
+      display(strToDisplay);
+      return;
+    }
+    if (val === "=") {
+      const lastChar = strToDisplay[strToDisplay.length - 1];
+      if (operators.includes(lastChar)) {
+        strToDisplay = strToDisplay.slice(0, -1);
+      }
+      return total();
+    }
+
+    if (val === "C") {
+      strToDisplay = strToDisplay.slice(0, -1);
+      return display(strToDisplay);
+    }
+
+    if (operators.includes(val)) {
+      lastOperator = val;
+      const lastChar = strToDisplay[strToDisplay.length - 1];
+      if (operators.includes(lastChar)) {
+        strToDisplay = strToDisplay.slice(0, 1);
+        strToDisplay += val;
+        return display(strToDisplay);
+      }
+    }
+
+    if (val === ".") {
+      if (lastOperator) {
+        const operatorIndex = strToDisplay.lastIndexOf(lastOperator);
+
+        const lastNumberSet = strToDisplay.slice(operatorIndex + 1);
+        if (lastNumberSet.includes(".")) {
+          return;
+        }
+      }
+      if (!lastOperator && strToDisplay.includes(".")) return;
+    }
+
+    // if (strToDisplay.includes(".")) return;
     strToDisplay += val;
     display(strToDisplay);
   });
@@ -20,4 +66,11 @@ buttonsArr.map((btns) => {
 
 const display = (str) => {
   displayEl.value = str;
+};
+
+const total = () => {
+  const ttl = eval(strToDisplay);
+  display(ttl);
+  /////converting to the string because slice methodd does not work in number ////
+  strToDisplay = ttl.toString();
 };
